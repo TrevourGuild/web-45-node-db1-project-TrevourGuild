929d3660ceda88df5eb90ca2beffe0d71a523026
TrevourGuild <trevorr.guildd@gmail.com>
2021-09-06 11:35:29 -0700

    changed router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,32 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  res.json('get')
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  res.json('get')
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  res.json('get')
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  res.json('get')
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  res.json('get')
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.json('get')
 })
 
 module.exports = router;
